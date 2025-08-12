3f7e695b1bdfc13356994a89911c99dbff45ce05
Pavel <pavel.oliynyk@gmail.com>
2025-08-12 19:01:50 +0100

    feat: initial commit

--- modules/example/src/main/scala/Main.scala
+++ modules/example/src/main/scala/Main.scala
@@ -1,3 +1,29 @@
-class Main {
+import smithy4s.country._
+import cats.effect._
+import cats.implicits._
+import org.http4s.implicits._
+import org.http4s.ember.server._
+import org.http4s._
+import com.comcast.ip4s._
+import doobie.implicits.toSqlInterpolator
+import doobie.{ConnectionIO, Transactor}
+import doobie.util.transactor.Transactor.Aux
+import smithy4s.http4s.SimpleRestJsonBuilder
+import doobie.implicits._
+import services.Routes
+
+
+object Main extends IOApp.Simple {
+
+  val run = Routes.all
+    .flatMap { routes =>
+      EmberServerBuilder
+        .default[IO]
+        .withPort(port"9000")
+        .withHost(host"localhost")
+        .withHttpApp(routes.orNotFound)
+        .build
+    }
+    .use(_ => IO.never)
 
 }
--- modules/example/src/main/scala/services/CountryService.scala
+++ modules/example/src/main/scala/services/CountryService.scala
@@ -1,3 +1,55 @@
-class CountryService {
+package services
 
+import cats.effect.{IO, Resource}
+import doobie.util.transactor.Transactor.Aux
+import org.http4s.HttpRoutes
+import smithy4s.country.{Country, CountryService}
+import smithy4s.http4s.SimpleRestJsonBuilder
+import smithy4s.country._
+import cats.effect._
+import cats.implicits._
+import org.http4s.implicits._
+import org.http4s.ember.server._
+import org.http4s._
+import com.comcast.ip4s._
+import doobie.implicits.toSqlInterpolator
+import doobie.{ConnectionIO, Transactor}
+import doobie.util.transactor.Transactor.Aux
+import smithy4s.http4s.SimpleRestJsonBuilder
+import doobie.implicits._
+
+object CountryImpl extends CountryService[IO] {
+
+  private val transactor: Resource[IO, Aux[IO, Unit]] = {
+    Resource.pure(
+      Transactor.fromDriverManager[IO] (
+        driver = "org.postgresql.Driver",
+        url = "jdbc:postgresql://localhost:5432/geo",
+        user = "postgres",
+        password = "avel_pass",
+        logHandler = None
+      )
+    )
+  }
+
+  def find(name: String): ConnectionIO[Option[Country]] =
+    sql"select code, name from country where name = $name".query[Country].option
+
+  def countryOps(name: String): IO[Country] = {
+    transactor.use { conn =>
+      for {
+        country <- find(name).transact(conn)
+      } yield country.get // or throw Error
+    }
+  }
+}
+
+object Routes {
+  private val example: Resource[IO, HttpRoutes[IO]] =
+    SimpleRestJsonBuilder.routes(CountryImpl).resource
+
+  private val docs: HttpRoutes[IO] =
+    smithy4s.http4s.swagger.docs[IO](CountryService)
+
+  val all: Resource[IO, HttpRoutes[IO]] = example.map(_ <+> docs)
 }
